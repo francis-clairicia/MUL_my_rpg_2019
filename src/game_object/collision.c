f7d7a873bd2c67fdd7f8a8e12a6cc0c1ed220c21
matheo.lucak <matheo.lucak@epitech.eu>
2020-04-28 02:10:43 +0200

    [ERROR HANDLING] AABB COLLISION: check if mass = 0 befor getting the invert of the mass

--- src/game_object/collision.c
+++ src/game_object/collision.c
@@ -9,20 +9,6 @@
 #include "math_process.h"
 #include "game_object.h"
 
-static sfBool game_object_special_collision_solving(game_obj_t *obj1,
-                                                    game_obj_t *obj2,
-                                                    sfVector2f depth)
-{
-    if (obj1->body.mass == __FLT_MAX__ && obj2->body.mass == __FLT_MAX__)
-        return (sfFalse);
-    if (obj1->body.mass == __FLT_MAX__) {
-        obj2->body.acc = vec_add(obj2->body.acc, vec_mult(depth, -1));
-    } else if (obj2->body.mass == __FLT_MAX__) {
-        obj1->body.acc = vec_add(obj1->body.acc, depth);
-    }
-    return (sfTrue);
-}
-
 static sfBool game_object_collision_solving(game_obj_t *obj1,
                                                 game_obj_t *obj2)
 {
@@ -32,6 +18,8 @@ static sfBool game_object_collision_solving(game_obj_t *obj1,
     sfVector2f impulse;
     sfVector2f normal = vec_norm(rv);
 
+    if (!(obj1->body.mass) || !(obj2->body.mass))
+        return (sfFalse);
     j = -(1 + 0.8) * rv_magn;
     j /= (1 / obj1->body.mass) + (1 / obj2->body.mass);
     impulse = vec_mult(normal, j);
