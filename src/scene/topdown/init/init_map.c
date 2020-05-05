625e9a6489ee39de36427b4ad691000649890620
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-05-05 21:58:21 +0200

    [NO_MERGE] Just a commit for merging

--- src/scene/topdown/init/init_map.c
+++ src/scene/topdown/init/init_map.c
@@ -8,18 +8,6 @@
 #include "init_topdown.h"
 #include "loader.h"
 
-static load_config_t map_config[] =
-{
-    {"WATER", 0, WATER},
-    {"SAND", 0, SAND},
-    {"DIRT", 0, DIRT},
-    {"BUOY1", 0, BUOY1},
-    {"BUOY2", 0, BUOY2},
-    {"BUOY3", 0, BUOY3},
-    {"BUOY4", 0, BUOY4},
-    {NULL, 0, -1}
-};
-
 static sfBool set_topdown_map_size(topdown_t *topdown)
 {
     game_obj_t *highest_x = find_game_object_highest_x(topdown->map);
@@ -33,7 +21,8 @@ static sfBool set_topdown_map_size(topdown_t *topdown)
 
 sfBool init_topdown_map(topdown_t *topdown)
 {
-    topdown->map = load_config_from_file("assets/config/map", map_config);
+    topdown->map = load_file_with_config("assets/config/map", map_config);
+
     if (!topdown->map)
         return (sfFalse);
     if (!set_topdown_map_size(topdown))
